ff45925fc221031061c11cc1b11d08e9553dcb47
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-10-08 15:48:42 -0300

    add controller and configurations

---
+++ Todo/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/teste")]
+        public string Get()
+        {
+            return "Hello Word!";
+        }
+    }
+}
